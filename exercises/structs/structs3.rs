d788c1ddf3e18ebb2bb91ac6951e88ab2608ab35
Myles Borins <myles.borins@gmail.com>
2020-08-20 19:46:25 -0400

    done quiz two

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -1,9 +1,7 @@
-// structs3.rs
+ // structs3.rs
 // Structs contain more than simply some data, they can also have logic, in this
 // exercise we have defined the Package struct and we want to test some logic attached to it,
 // make the code compile and the tests pass! If you have issues execute `rustlings hint structs3`
-
-// I AM NOT DONE
 
 #[derive(Debug)]
 struct Package {
@@ -15,18 +13,18 @@ struct Package {
 impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
-            // Something goes here...
+            panic!("Package cannot be weightless");
         } else {
             return Package {sender_country, recipient_country, weight_in_grams};
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        return self.sender_country != self.recipient_country;
     }
 
-    fn get_fees(&self, cents_per_kg: i32) -> ??? {
-        // Something goes here... (beware of grams to kg conversion)
+    fn get_fees(&self, cents_per_kg: i32) -> i32 {
+        return (self.weight_in_grams / 1000) * cents_per_kg;
     }
 }
 
@@ -58,7 +56,7 @@ mod tests {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Spain");
 
-        let cents_per_kg = ???;
+        let cents_per_kg = 4500;
         
         let package = Package::new(sender_country, recipient_country, 1500);
         
